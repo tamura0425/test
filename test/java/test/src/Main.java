ffe0abced4e43fd518096866dd4a5e9c58ed6c56
“hirotada tamura <tamura0425@gmail.com>
2022-10-14 00:25:25 +0900

    git 整理-12

--- test/java/test/src/Main.java
+++ test/java/test/src/Main.java
@@ -14,6 +14,6 @@ public class Main {
         System.out.println("Hello world!11");
         System.out.println("Hello world!12");
         System.out.println("Hello world!13");
-
+        System.out.println("Hello world!14");
     }
 }
