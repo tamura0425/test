4738f9f037d922cb32eef36f7ccda40ff96fb6bc
“hirotada tamura <tamura0425@gmail.com>
2022-10-13 19:45:26 +0900

    git 整理-2

--- test/java/test/src/Main.java
+++ test/java/test/src/Main.java
@@ -1,18 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-<<<<<<< HEAD
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-=======
->>>>>>> origin/develop
+        System.out.println("Hello world!1");
+        System.out.println("Hello world!2");
+        System.out.println("Hello world!3");
+        System.out.println("Hello world!4");
     }
 }
