3e1cd87ed7fb3314fd037f28bdfe1b23f557b5c3
“hirotada tamura <tamura0425@gmail.com>
2022-10-14 00:28:30 +0900

    git 整理-16

--- test/java/test/src/Main.java
+++ test/java/test/src/Main.java
@@ -6,15 +6,15 @@ public class Main {
         System.out.println("Hello world!3");
         System.out.println("Hello world!4");
         System.out.println("Hello world!5");
-//        System.out.println("Hello world!6");
-//        System.out.println("Hello world!7");
-//        System.out.println("Hello world!8");
-//        System.out.println("Hello world!9");
-//        System.out.println("Hello world!10");
-//        System.out.println("Hello world!11");
-//        System.out.println("Hello world!12");
-//        System.out.println("Hello world!13");
-//        System.out.println("Hello world!14");
-//        System.out.println("Hello world!15");
+        System.out.println("Hello world!6");
+        System.out.println("Hello world!7");
+        System.out.println("Hello world!8");
+        System.out.println("Hello world!9");
+        System.out.println("Hello world!10");
+        System.out.println("Hello world!11");
+        System.out.println("Hello world!12");
+        System.out.println("Hello world!13");
+        System.out.println("Hello world!14");
+        System.out.println("Hello world!15");
     }
 }
