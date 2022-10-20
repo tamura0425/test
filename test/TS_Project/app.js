4c497bd65dead7dc1e21101b427eceb4bd49a42a
“hirotada tamura <tamura0425@gmail.com>
2022-10-21 01:58:07 +0900

    typ　型　練習

--- test/TS_Project/app.js
+++ test/TS_Project/app.js
@@ -1,3 +1,6 @@
 console.log('Hello TypeScript');
 console.log('Hello TypeScript');
+var a = 3;
+var b = "hiro";
 console.log(10 + 10);
+console.log(b);
--- test/TS_Project/app.ts
+++ test/TS_Project/app.ts
@@ -1,4 +1,9 @@
 console.log('Hello TypeScript');
 console.log('Hello TypeScript');
 
+const a: number = 3;
+
+const b: string = "hiro";
+
 console.log(10+10);
+console.log(b);
