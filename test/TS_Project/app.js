776674f36fbedb6cb3c77f87ae0cdf1e92c65b79
“hirotada tamura <tamura0425@gmail.com>
2022-10-23 00:55:18 +0900

    Co-authored-by: “hirotada tamura <“tamura0425@gmail.com>

--- test/TS_Project/app.js
+++ test/TS_Project/app.js
@@ -1,6 +1,16 @@
 console.log('Hello TypeScript');
 console.log('Hello TypeScript');
+console.log('Hello TypeScript');
+console.log('Hello TypeScript');
 var a = 3;
 var b = "hiro";
 console.log(10 + 10);
 console.log(b);
+var Test2 = /** @class */ (function () {
+    function Test2(initName) {
+        this.name = initName;
+        console.log(this.name);
+    }
+    return Test2;
+}());
+new Test2("55555555");
--- test/TS_Project/app.ts
+++ test/TS_Project/app.ts
@@ -1,5 +1,7 @@
 console.log('Hello TypeScript');
 console.log('Hello TypeScript');
+console.log('Hello TypeScript');
+console.log('Hello TypeScript');
 
 const a: number = 3;
 
@@ -7,3 +9,13 @@ const b: string = "hiro";
 
 console.log(10+10);
 console.log(b);
+
+
+class Test2{
+    name: string;
+    constructor(initName: string){
+        this.name = initName;
+        console.log(this.name);
+    }
+}
+new Test2("55555555");
